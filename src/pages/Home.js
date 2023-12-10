f76ef6b484bf3630f464e7f2b4e49545aa79b8a1
Alailson Barbosa <alailsonbcsilva@gmail.com>
2023-12-10 12:09:44 -0300

    inclusao da tela de uploads

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Home() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
   return (
     <div className="home">
-      <h1>Home</h1>
+      <div className="main-upload">
+        <h1 className="upload-title">Selecione um arquivo.</h1>
+        <label className="input-btn-label" for="input-btn">
+          Escolha o arquivo.
+        </label>
+        <input type="file" id="input-btn" onChange={handleFileChange} />
+        {selectedFile && (
+          <div className="doc-preview">
+            <h2>Preview do documento.</h2>
+            <img
+              src={URL.createObjectURL(selectedFile)}
+              alt="Preview"
+              className="image-preview"
+            />
+          </div>
+        )}
+      </div>
+      <div className="extraction">
+        <div className="inner-extraction">
+          <h1>TESTE</h1>
+        </div>
+      </div>
     </div>
   );
 }
